c1dfc723b9e9301a0241af6f021bbafb163b8710
Koushik-Sarker-Seemanto <koushik24@student.sust.edu>
2020-07-03 22:27:19 +0600

    repository service added

--- WebService/Startup.cs
+++ WebService/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Repositories;
 
 namespace WebService
 {
@@ -23,6 +25,14 @@ namespace WebService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<DatabaseSettings>(
+                Configuration.GetSection(nameof(DatabaseSettings)));
+            
+            services.AddSingleton<IDatabaseSettings>(sp => 
+                sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
+
+            services.AddSingleton<IMongoRepository, MongoRepository>();
+            
             services.AddControllersWithViews();
         }
 
