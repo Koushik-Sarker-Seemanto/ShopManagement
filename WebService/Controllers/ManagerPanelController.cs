2df4bbd137e4e408cd2fd9b20c9d86e349f4c9e0
Koushik-Sarker-Seemanto <koushik24@student.sust.edu>
2020-07-11 15:37:38 +0600

    manager panel setup complete

--- WebService/Controllers/ManagerPanelController.cs
+++ WebService/Controllers/ManagerPanelController.cs
@@ -1,15 +1,53 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models.AdminAuthModels;
+using Newtonsoft.Json;
+using Services.Contracts;
 
 namespace WebService.Controllers
 {
     [Authorize(Policy = "Manager")]
     public class ManagerPanelController : Controller
     {
+        private IUserServices _userServices;
+        private ILogger<AdminPanelController> logger;
+        //private IAdminPanelService _adminPanelService;
+
+        public ManagerPanelController(IUserServices userServices, ILogger<AdminPanelController> logger)
+        {
+            _userServices = userServices;
+            this.logger = logger;
+        }
         // GET
         public IActionResult Index()
         {
             return View();
+        }
+        
+        public IActionResult AddSeller()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddSeller([Bind] RegisterViewModel model)
+        {
+            if(ModelState.IsValid == false)
+            {
+                return View(model);
+            }
+            var result = await _userServices.RegisterUser(model);
+            logger.LogInformation($"RegisterViewModel: {JsonConvert.SerializeObject(result)}");
+            if (result!=null)
+            {
+                return RedirectToAction("Index", "ManagerPanel");
+            }
+            
+            ModelState.AddModelError("", "Invalid Registration");
+            return View(model);
         }
     }
 }
