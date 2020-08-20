1d963d271018ed6c76007a1d5cfeea8bc1329074
Koushik-Sarker-Seemanto <koushik24@student.sust.edu>
2020-08-20 17:34:46 +0600

    some fix

--- WebService/wwwroot/Admin/js/admin-scripts.js
+++ WebService/wwwroot/Admin/js/admin-scripts.js
@@ -29,7 +29,7 @@
 
     $(document).ready(function() {
         $('#managerDataTable').DataTable();
-//        $('#sellerDataTable').DataTable();
+        $('#sellerDataTable').DataTable();
     });
     
     $("#manager-table-header").click(function () {
